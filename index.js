5f19370e65658b9fd1a71e8755a7bbec4b64f783
Dima Dub <dzmitry.duboyski@gmail.com>
2023-05-01 07:20:39 +0400

    feat: add solving reCAPTCHA

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
 import puppeteer from "puppeteer";
 const APIKEY = process.env.APIKEY;
+import { Solver } from "2captcha-ts";
+const solver = new Solver(APIKEY);
 
-;(async () => {
+(async () => {
   const browser = await puppeteer.launch({
     headless: false,
   });
-  const [page] = await browser.pages();
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1080, height: 1024 });
 
   await page.goto("https://2captcha.com/demo/recaptcha-v2");
-  await page.waitForTimeout(5000);
+  await page.waitForSelector(".g-recaptcha");
 
+  // Extract the `sitekey` parameter from the page.
+  const sitekey = await page.evaluate(() => {
+    return document.querySelector(".g-recaptcha").getAttribute("data-sitekey");
+  });
+
+  // Get actual page url
+  const pageurl = await page.url();
+
+  // Submitting the captcha for solution to the service
+  const res = await solver.recaptcha({
+    pageurl: pageurl,
+    googlekey: sitekey,
+  });
+
+  console.log(res);
+
+  // Getting a captcha response including a captcha answer
+  const captchaAnswer = res.data;
+
+  // Use captcha answer
+  const setAnswer = await page.evaluate((captchaAnswer) => {
+    // It is not necessary to make this block visible, it is done here for clarity.
+    document.querySelector("#g-recaptcha-response").style.display = "block";
+    document.querySelector("#g-recaptcha-response").value = captchaAnswer;
+  }, captchaAnswer);
+
+  // Press the button to check the result.
+  await page.click('button[type="submit"]');
+
+  // Check result
+  await page.waitForSelector("form div pre code");
+
+  const resultBlockSelector = "form div pre code";
+  let statusSolving = await page.evaluate((selector) => {
+    return document.querySelector(selector).innerText;
+  }, resultBlockSelector);
+
+  statusSolving = JSON.parse(statusSolving);
+  if (statusSolving.success) {
+    console.log("Captcha solved successfully!!!");
+  }
+
+  await page.waitForTimeout(5000);
   browser.close();
 })();
